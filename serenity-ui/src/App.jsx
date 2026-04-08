3022a0029149ae3c92fd06eb56349e8b6fbb5b01
Rushika080 <222262123+Rushika080@users.noreply.github.com>
2026-04-08 20:38:53 +0530

    Update App.jsx

--- serenity-ui/src/App.jsx
+++ serenity-ui/src/App.jsx
@@ -360,7 +360,7 @@ function BoxBreathing() {
     };
     ref.current = setTimeout(tick, 1000);
     return () => clearTimeout(ref.current);
-  }, [running]);
+  }, [phases]);
   function toggle() {
     if (running) {
       clearTimeout(ref.current);
